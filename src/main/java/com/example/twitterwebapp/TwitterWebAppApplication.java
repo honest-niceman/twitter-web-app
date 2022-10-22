dffa1e76220ded186d4a5dc275786b9c7ee8488d
Georgii Vlasov <honest.niceman@gmail.com>
2022-10-22 22:43:10 +0400

    Init commit

---
+++ src/main/java/com/example/twitterwebapp/TwitterWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.twitterwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterWebAppApplication{
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterWebAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/twitterwebapp/TwitterWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twitterwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
